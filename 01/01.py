363f26c280d80e3a465338ad8e32632fd455f1bb
Christian Cwienk <christian.cwienk@sap.com>
2022-12-01 09:31:24 +0100

    solve 1a

--- 01/01.py
+++ 01/01.py
@@ -4,8 +4,20 @@ import util
 
 
 def main():
+    max_sum = 0
+
+    current_sum = 0
+
     with open(util.input_file) as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                current_sum += int(line)
+                max_sum = max(max_sum, current_sum)
+            else: # reached end of block
+                current_sum = 0
+
+    print(max_sum)
 
 
 if __name__ == '__main__':
