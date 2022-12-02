8a33334f180511b494b441b274cc0ade6e82345c
Christian Cwienk <christian.cwienk@sap.com>
2022-12-02 09:25:55 +0100

    solve 02a

--- 02/02.py
+++ 02/02.py
@@ -2,10 +2,61 @@
 
 import util
 
+rock = 'A', 'X'
+paper = 'B', 'Y'
+scissors = 'C', 'Z'
+
+
+def calculate_score(opponent, reply):
+    if reply in rock:
+        score = 1
+    elif reply in paper:
+        score = 2
+    elif reply in scissors:
+        score = 3
+    else:
+        raise ValueError(reply)
+
+    if opponent in rock:
+        if reply in scissors:
+            return score + 0 # we lost
+        elif reply in paper:
+            return score + 6
+        elif reply in rock:
+            return score + 3 # draw
+        else:
+            raise ValueError(opponent)
+    elif opponent in paper:
+        if reply in rock:
+            return score + 0 # we lost
+        elif reply in scissors:
+            return score + 6
+        elif reply in paper:
+            return score + 3 # draw
+        else:
+            raise ValueError(opponent)
+    elif opponent in scissors:
+        if reply in paper:
+            return score + 0 # we lost
+        elif reply in rock:
+            return score + 6
+        elif reply in scissors:
+            return score + 3 # draw
+        else:
+            raise ValueError(opponent)
+    else:
+        raise ValueError(opponent)
+
 
 def main():
+    score = 0
     with open(util.input_file) as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            opponent, reply = line.split(' ')
+            score += calculate_score(opponent, reply)
+
+    print(score)
 
 
 if __name__ == '__main__':
