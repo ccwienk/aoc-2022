d6b514eccb45540207635fea5686bc4687c32152
Christian Cwienk <christian.cwienk@sap.com>
2022-12-03 09:32:28 +0100

    solve 03a

--- 03/03.py
+++ 03/03.py
@@ -3,9 +3,30 @@
 import util
 
 
+def priority(char: str):
+    if char.islower():
+        return ord(char) - 96
+    if char.isupper():
+        return ord(char) - 38
+
+
 def main():
+    prioty_sum = 0
+
     with open(util.input_file) as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            left, right = line[0: (l:=int(len(line)/2))], line[l:]
+
+            left_charset = set(c for c in left)
+            right_charset = set(c for c in right)
+
+            common_chars = left_charset & right_charset
+
+            for cc in common_chars:
+                prioty_sum += priority(cc)
+
+    print(f'{prioty_sum=}')
 
 
 if __name__ == '__main__':
