3504032ba2b4a8c5dfaa275cfaac41bf1af38ff1
Christian Cwienk <christian.cwienk@sap.com>
2022-12-04 06:25:40 +0100

    solve 04a

--- 04/04.py
+++ 04/04.py
@@ -3,9 +3,29 @@
 import util
 
 
+def rangeset(rangestr):
+    left, right = rangestr.split('-')
+    left = int(left)
+    right = int(right)
+
+    return set(range(left, right + 1))
+
+
 def main():
+    subset_pairs = 0
+
     with open(util.input_file) as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            left, right = line.split(',')
+
+            leftset = rangeset(left)
+            rightset = rangeset(right)
+
+            if leftset.issubset(rightset) or rightset.issubset(leftset):
+                subset_pairs += 1
+
+    print(subset_pairs)
 
 
 if __name__ == '__main__':
