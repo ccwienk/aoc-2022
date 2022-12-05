a0b92dcdfbaf52f3f9c7f32de5db41b7045cf659
Christian Cwienk <christian.cwienk@sap.com>
2022-12-05 09:44:12 +0100

    solve 05a

--- 05/05.py
+++ 05/05.py
@@ -3,10 +3,54 @@
 import util
 
 
-def main():
-    with open(util.input_file) as f:
-        pass
+def iter_crates(line: str):
+    line = line.removesuffix('\n')
 
+    for offset in range(1, 9 * 4, 4):
+        if offset >= len(line):
+            yield None
+            continue
+
+        crate = line[offset]
+        if crate == ' ':
+            yield None
+        else:
+            yield crate
+
+
+def main():
+    cratestacks = [[] for _ in range(9)] # idx 0: bottom
+
+    processing_crates = True
+
+    with open(util.input_file) as f:
+        for line in f.readlines():
+            if processing_crates:
+                if not line.startswith('['):
+                    processing_crates = False
+                    # skip numbers + empty line
+                    f.readline()
+                else:
+                    for idx, c in enumerate(iter_crates(line)):
+                        if c:
+                            cratestacks[idx].insert(0, c)
+                continue
+
+            line = line.strip()
+            if not line:
+                continue
+
+            _, count, _, src, _, tgt = line.split(' ')
+            count = int(count)
+            src = cratestacks[int(src) - 1]
+            tgt = cratestacks[int(tgt) - 1]
+
+            for _ in range(count):
+                tgt.append(src.pop())
+
+        for c in cratestacks:
+            print(c[-1], end='')
+        print()
 
 if __name__ == '__main__':
     main()
