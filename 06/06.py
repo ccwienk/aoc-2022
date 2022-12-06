a5a1c430c63492e53857e3c909f7cad1ea8998f2
Christian Cwienk <christian.cwienk@sap.com>
2022-12-06 09:12:33 +0100

    solve 06a

--- 06/06.py
+++ 06/06.py
@@ -5,7 +5,15 @@ import util
 
 def main():
     with open(util.input_file) as f:
-        pass
+        chars = f.read().strip()
+
+    for idx in range(len(chars) - 4):
+        current = chars[idx: idx + 4]
+        if len(set(current)) < 4:
+            continue
+        break # reached start-of-package-marker
+
+    print(idx + 4)
 
 
 if __name__ == '__main__':
