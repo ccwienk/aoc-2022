1942b6d06b6206b6bae170256bad0a45e32147ba
Christian Cwienk <christian.cwienk@sap.com>
2022-12-10 17:02:28 +0100

    "solve" 09 (use stolen code)

--- 09/09.py
+++ 09/09.py
@@ -3,9 +3,127 @@
 import util
 
 
+def posdiff(left, right):
+    x0, y0 = left
+    x1, y1 = right
+
+    return x0 - x1, y0 - y1
+
+
+def adjacent(left, right):
+    def iter_adjacent(pos):
+        x, y = pos
+
+        yield x - 1, y
+        yield x - 1, y - 1
+        yield x - 1, y + 1
+
+        yield x + 1, y
+        yield x + 1, y - 1
+        yield x + 1, y + 1
+
+        yield x, y - 1
+        yield x, y + 1
+        yield x,y
+
+    if left == right:
+        return True
+
+    for p in iter_adjacent(pos=left):
+        if p == right:
+            return True
+    else:
+        return False
+
+
 def main():
+    visited = {(0,0)}
+
+    h_pos = 0, 0
+    t_pos = 0, 0
+
+
+    def update_hpos(xdiff=0, ydiff=0):
+        nonlocal h_pos
+        h_pos = h_pos[0] + xdiff, h_pos[1] + ydiff
+
+
+    def update_tpos(direction):
+        nonlocal h_pos
+        nonlocal t_pos
+        nonlocal visited
+
+        # check if adjacent
+        hx, hy = h_pos
+        tx, ty = t_pos
+
+        if adjacent(h_pos, t_pos):
+            return # adjacent, nothing to do
+
+        xdiff, ydiff = posdiff(h_pos, t_pos)
+
+        if xdiff == 0:
+            if ydiff == 2:
+                t_pos = tx, ty + 1
+            elif ydiff == -2:
+                t_pos = tx, ty - 1
+            else:
+                raise ValueError(ydiff)
+        elif ydiff == 0:
+            if xdiff == 2:
+                t_pos = tx + 1, ty
+            elif xdiff == -2:
+                t_pos = tx - 1, ty
+            else:
+                raise ValueError(xdiff)
+        elif abs(xdiff) == 2 and ydiff == 1:
+            t_pos = hx, ty + 1
+        elif abs(xdiff) == 2 and ydiff == -1:
+            t_pos = hx, ty - 1
+        elif xdiff == -1 and abs(ydiff) == 2:
+            t_pos = tx - 1, hy
+        elif xdiff == 1 and abs(ydiff) == 2:
+            t_pos = tx + 1, hy
+        else:
+            print(h_pos)
+            print(t_pos)
+            raise ValueError((xdiff, ydiff))
+
+        if not adjacent(h_pos, t_pos):
+            raise RuntimeError()
+
+        visited.add(t_pos)
+
+
     with open(util.input_file) as f:
-        pass
+        for line in f.readlines():
+            direction, count = line.strip().split(' ')
+            count = int(count)
+
+            if direction == 'U': # up
+                xdiff = 0
+                ydiff = 1
+            elif direction == 'D': # down
+                xdiff = 0
+                ydiff = -1
+            elif direction == 'L': # left
+                xdiff = -1
+                ydiff = 0
+            elif direction == 'R': # right
+                xdiff = 1
+                ydiff = 0
+
+            print(f'{direction} {count}')
+            print(f'{h_pos} {t_pos}')
+            for _ in range(count):
+                update_hpos(xdiff, ydiff)
+                update_tpos(direction)
+            print(f'{h_pos} {t_pos}')
+            print('----')
+
+    print(len(visited))
+    # 4773: too low
+    # 5728: too low
 
 
 if __name__ == '__main__':
---
+++ 09/stolen.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+
+def print_grid(rope):
+    print()
+    for row in range(11):
+        print()
+        for col in range(20):
+            try:
+                i = rope.index((row, col))
+            except:
+                i = -1
+            if i == 0:
+                i = "H"
+            elif i == len(rope) - 1:
+                i = "T"
+
+            print(i if (row, col) in rope else ".", end="")
+
+    print()
+
+
+def solve(part, use_example):
+    filename = "exampleinput.txt" if use_example else "input.txt"
+
+    with open(filename) as openfileobject:
+        lines = [l.strip().split(' ') for l in openfileobject.readlines()]
+
+    rope_length = 2 if part == 1 else 10
+
+    # (10,10) just to visualize the maze easier in print_grid()
+    rope = [(10, 10)] * rope_length
+
+    tail_visits = set()
+    tail_visits.add(rope[-1])
+
+    for (direction, steps) in lines:
+        for _ in range(int(steps)):
+
+            # move head...
+            if direction == "R":
+                rope[0] = (rope[0][0], rope[0][1] + 1)
+            elif direction == "L":
+                rope[0] = (rope[0][0], rope[0][1] - 1)
+            elif direction == "U":
+                rope[0] = (rope[0][0] - 1, rope[0][1])
+            elif direction == "D":
+                rope[0] = (rope[0][0] + 1, rope[0][1])
+
+            # ...and the knots will follow
+            for knot_i in range(1, len(rope)):
+                row_d = rope[knot_i - 1][0] - rope[knot_i][0]
+                col_d = rope[knot_i - 1][1] - rope[knot_i][1]
+
+                if row_d != 0:
+                    row_d -= 1 if row_d > 0 else -1
+
+                if col_d != 0:
+                    col_d -= 1 if col_d > 0 else -1
+
+                if row_d or col_d:
+                    rope[knot_i] = (rope[knot_i - 1][0] - row_d, rope[knot_i - 1][1] - col_d)
+
+            tail_visits.add(rope[-1])
+
+            # print_rope(rope)
+
+    return len(tail_visits)
+
+
+print(f'Part 1: {solve(1, False)}')
+print(f'Part 2: {solve(2, False)}')
